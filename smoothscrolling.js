45f0fadabe934a67ba9db9038b88787828f972e3
Chase King <chasechocolate@comcast.net>
2017-04-01 18:40:46 -0700

    Emojis!

--- smoothscrolling.js
+++ smoothscrolling.js
@@ -1,19 +1,21 @@
-var time = 500;
+var time = 600;
 
 $(function(){
-  $('a[href*="#"]:not([href="#"])').click(function(){
-    if(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname){
-      var target = $(this.hash);
+    $('a[href*="#"]:not([href="#"])').click(function(){
+        if(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname){
+            var target = $(this.hash);
 
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 
-      if(target.length){
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, time);
+            if(target.length){
 
-        return false;
-      }
-    }
-  });
+
+                $("html, body").animate({
+                    scrollTop: target.offset().top - parseInt(target.css("marginTop"))
+                }, time);
+
+                return false;
+            }
+        }
+    });
 });
