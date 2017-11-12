3219495cba35b0a4b1bd40689434f6fd013246ce
Chase King <chasechocolate@comcast.net>
2017-11-12 10:40:10 -0800

    Fancy schmancy typing page headers

--- school/hci-ai/index.js
+++ school/hci-ai/index.js
@@ -4,7 +4,6 @@ $(".animate-typing").each(function(){
     var contents = div.find(".animate-typing-text");
     var cursor = $("<span class='cursor'></span>");
     var cursorVisible = true;
-    var lastType = -1;
 
     //Save the text in the headings
     var contentsText = [];
@@ -15,24 +14,6 @@ $(".animate-typing").each(function(){
         span.text("");
     }
 
-    function updateCursor(){
-        if(Date.now() - lastType < 500){
-            if(!cursorVisible){
-                cursorVisible = true;
-                cursor.fadeIn(0);
-            }
-        } else {
-            cursorVisible = !cursorVisible;
-
-            if(cursorVisible){
-                cursor.fadeIn(cursorBlink);
-            } else {
-                cursor.fadeOut(cursorBlink);
-            }
-        }
-    }
-
-    setInterval(updateCursor, cursorBlink);
     typeHeading(0);
 
     function typeHeading(headingIndex){
@@ -64,8 +45,8 @@ $(".animate-typing").each(function(){
 
         function addText(){
             span.text(fullText.substring(0, span.text().length + 1));
-            lastType = Date.now();
-            updateCursor();
+            cursor.data("lastType", Date.now());
+            updateCursor(cursor);
 
             if(span.text().length < fullText.length){
                 setTimeout(addText, 65 + Math.random() * 80);
@@ -78,24 +59,100 @@ $(".animate-typing").each(function(){
     }
 });
 
-function setCurrentPage(index){
+//Cursor blink
+function updateCursor(cursor){
+    var lastType = cursor.data("lastType");
+    var cursorVisible = cursor.data("isVisible");
+
+    if(!lastType || Date.now() - lastType < 500){
+        if(!cursorVisible){
+            cursorVisible = true;
+            cursor.fadeIn(0);
+        }
+    } else {
+        cursorVisible = !cursorVisible;
+
+        if(cursorVisible){
+            cursor.fadeIn(cursorBlink);
+        } else {
+            cursor.fadeOut(cursorBlink);
+        }
+    }
+
+    cursor.data("isVisible", cursorVisible);
+}
+
+setInterval(function(){
+    $(".cursor").each(function(){
+        updateCursor($(this));
+    });
+}, cursorBlink);
+
+var pageHeadingAnimateIndex = -1;
+$(".section-content h2").each(function(){
+    var h2 = $(this);
+    h2.data("text", h2.text());
+    h2.empty();
+
+    var textSpan = $("<span class='text'></span>");
+    var cursor = $("<span class='cursor'></span>");
+    h2.append(textSpan);
+    h2.append(cursor);
+});
+
+function setCurrentPage(index, scroll){
     var section = $(".section-content[data-page=" + index + "]");
 
     $(".step[data-page=" + index + "]").addClass("active").siblings().removeClass("active");
     section.addClass("active").siblings().removeClass("active");
     $(".line-progress").css("width", ((100 / ($(".section-content").length - 1)) * (index - 1)) + "%");
 
-    $("html, body").animate({
-        scrollTop: section.offset().top - 100
-    }, 800);
+    //Scroll to the top heading
+    if(scroll){
+        $("html, body").animate({
+            scrollTop: section.offset().top - 100
+        }, 800);
+    }
+
+    //Animate typing of the top heading
+    pageHeadingAnimateIndex = index;
+    var h2 = section.find("h2");
+    var fullText = h2.data("text");
+    var textSpan = h2.find("span.text");
+    var cursor = h2.find("span.cursor");
+
+    //Clear the header text
+    textSpan.text("");
+
+    //Start typing after a 500ms delay
+    setTimeout(function(){
+        addText();
+    }, 500);
+
+    function addText(){
+        textSpan.text(fullText.substring(0, textSpan.text().length + 1));
+        cursor.data("lastType", Date.now());
+        updateCursor(cursor);
+
+        if(textSpan.text().length < fullText.length){
+            if(pageHeadingAnimateIndex == index){
+                //Only keep typing letters if we're on the same page
+                setTimeout(addText, 65 + Math.random() * 80);
+            }
+        } else {
+            //All done!
+        }
+    }
 }
 
+setCurrentPage(1, false);
+
 $(".step").click(function(){
-    setCurrentPage($(this).data("page"));
+    setCurrentPage($(this).data("page"), true);
 });
 
 $("button[data-page]").click(function(){
-    setCurrentPage($(this).data("page"));
+    setCurrentPage($(this).data("page"), true);
 });
 
 var nav = $(".timeline-container");
